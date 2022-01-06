d82df3b9405af2c77b5a3106a7c583740947a0bf
Mike Rudge <mikerudge@me.com>
2022-01-06 21:07:41 +0000

    adds tailwind ext

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen text-center justify-center items-center flex font-mono">
-      <div>
-
-     <h1 className="text-3xl font-bold text-gray-700 hover:text-gray-800">
-      Welcome to the GitPod Demo
-    </h1>
-      <p className='text-lg text-gray-500 mt-2'>
-        This is just a simple tailwind react app
-      </p>
+    <div className="flex justify-start items-center p-8 min-h-screen font-mono text-center bg-gray-100">
+      <div className='text-left'>
+        <h1 className="text-3xl font-bold text-gray-700 hover:text-gray-800">
+          Welcome to the GitPod Demo
+        </h1>
+        <p className='mt-2 text-lg text-gray-600'>
+          This is just a simple tailwind react app
+        </p>
       </div>
     </div>
   );
